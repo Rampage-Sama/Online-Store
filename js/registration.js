46ae2b6de1d126b45fddcf0c73f474e187ef5ece
Rendani007 <37965299+Rendani007@users.noreply.github.com>
2021-05-08 01:52:15 +0200

    Rendani
    the website is too big to commit so i added files manually

--- js/registration.js
+++ js/registration.js
@@ -6,34 +6,49 @@ const steps = Array.from(document.querySelectorAll("form .step"));
 const nextBtn = document.querySelectorAll("form .next-btn");
 const prevBtn = document.querySelectorAll("form .previous-btn");
 const regBtn = document.querySelectorAll("form .register-L");
-const form = document.querySelector("form");
+const form = document.getElementById("form");
+
+// this code is for the checkbox choice//
+function check(el){
+  var choice = document.getElementsByName("choose");
+  choice.forEach(function(item){
+    if(item!=el)
+      item.checked = false;
+  });
+}
 
 nextBtn.forEach((button) => {
   button.addEventListener("click", () => {
     changeStep("next");
   });
 });
+
 regBtn.forEach((button) => {
   button.addEventListener("click", () => {
     changeStep("next");
   });
 });
+
 prevBtn.forEach((button) => {
   button.addEventListener("click", () => {
     changeStep("prev");
   });
 });
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const inputs = [];
-  form.querySelectorAll("input").forEach((input) => {
-    const { name, value } = input;
-    inputs.push({ name, value });
-  });
-  console.log(inputs);
-  form.reset();
-});
+// submit button
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+
+
+//   const inputs = [];
+//   form.querySelectorAll("input").forEach((input) => {
+//     const { name, value } = input;
+//     inputs.push({ name, value });
+//   });
+//   console.log(inputs);
+//   form.reset();
+// });
 
 function changeStep(btn) {
   let index = 0;
@@ -48,36 +63,40 @@ function changeStep(btn) {
   steps[index].classList.add("active");
 }
 
-
-
-
-
+//connect the submit button with the code below
 
 //add event object gets passed when form is submitted
-// form.addEventListener('submit', function(event){
-//     event.preventDefault();//prevents form from auto submitting
+form.addEventListener('submit', function(event){
+    event.preventDefault();//prevents form from auto submitting
 
-    // login/register step 
+    //login/register step 
+    // step 1:Personal details 
 
-    //step 1:Personal details 
-
-    // var Name = document.getElementById("name").value.trim();
-    // console.log(Name);
-    // var Lastname = document.getElementById("lastname").value.trim();
-    // var Age = document.getElementById("age").value.trim();
-    // var Email = document.getElementById("email").value.trim();
-    // var Username = document.getElementById("username").value.trim();
-    // var Number = document.getElementById("mobilenumber").value.trim();
-    // var Password = document.getElementById("password").value.trim();
-    // console.log(Password);
-    // var Confirmpass = document.getElementById("confirmpassword").value.trim();
+    
+    var userName = document.getElementById("name").value.trim();
+    console.log(userName);
+    var Lastname = document.getElementById("lastname").value.trim();
+    console.log(Lastname);
+    var Age = document.getElementById("age").value.trim();
+    console.log(Age);
+    var birthday = document.getElementById("birthday").value.trim();
+    console.log(birthday);
+    var Email = document.getElementById("email").value.trim();
+    console.log(Email);
+    var cell = document.getElementById("cell").value.trim();
+    console.log(cell);
+    var Username = document.getElementById("username").value.trim();
+    console.log(Username);
+    var Password = document.getElementById("password").value.trim();
+    console.log(Password);
+    // var Confirmpass = document.getElementById("confirmPass").value.trim();
     // console.log(Confirmpass);
 
-    // form 2: Banking and Identification 
+// form 2: Banking and Identification 
      
-    // sending data inside an object
-// fetch
-// fetch('https://localhost:8080/Kutu/registerUser', {
+// sending data inside an object
+//  fetch
+// fetch('localhost:8080/Kutu/registerUser', {
 //     method: 'POST',
 //     body:JSON.stringify({
 //         "uName" : Name,
@@ -97,9 +116,9 @@ function changeStep(btn) {
 //     return response.json()
 // })
 // .then(function(data){
-//     //Console.log(data);
+    //Console.log(data);
 // })
 
-// })
+ })
 
 
